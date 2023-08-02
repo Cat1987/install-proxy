ee9f16323fd2ca439f33b727ba17f5e5900719c3
Cat1987 <2224371900@qq.com>
2023-08-02 15:09:43 +0800

    add nginx installation

--- nginx/install.sh
+++ nginx/install.sh
@@ -14,8 +14,10 @@ InstallNginx(){
   sudo rpm -Uvh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
   yum install nginx -y
   service nginx start
+  systemctl enable nginx.service
 }
 
+# This function must been after InstallNginx
 ConfigureLoadBalance(){
   mkdir -p /etc/nginx/tcpconf.d
   echo "stream {
@@ -33,6 +35,17 @@ ConfigureLoadBalance(){
     echo "$val"
     sed -i "3i $val;" /etc/nginx/tcpconf.d/ssrproxy.conf
   done
+  echo "include /etc/nginx/tcpconf.d/*.conf;" >> /etc/nginx/nginx.conf
 }
 
+OptimizeNginxConf(){
+  sed -i "1i worker_rlimit_nofile 51200;" /etc/nginx/nginx.conf
+  #update work connections
+  sed -i "s/worker_connections  1024;/worker_connections  10240;/g" /etc/nginx/nginx.conf
+  nginx -t
+  nginx -s reload
+}
+
+InstallNginx
 ConfigureLoadBalance
+OptimizeNginxConf
